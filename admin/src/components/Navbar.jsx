ca24b334a878bdadff9af14e785578bf57598387
yohan_mr <yohanmri.21@uom.lk>
2025-07-13 19:00:11 +0530

    Logo Change

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -23,10 +23,12 @@ const Navbar = () => {
     return (
         <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
             <div className='flex items-center gap-2 text-xs'>
-                <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt='' />
+                <img className='w-[50px] sm:w-[50px] cursor-pointer' src={assets.admin_logo} alt='' />
                 <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Lawyer'} </p>
             </div>
-            <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
+            <button onClick={logout} className=' text-white text-sm px-10 py-2 rounded-full'
+                style={{ background: 'linear-gradient(to right, #D00C1F, #6A0610)' }}
+            >Logout</button>
         </div>
     )
 }
