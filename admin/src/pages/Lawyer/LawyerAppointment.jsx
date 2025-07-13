619f58a6b1e0ab907fadab21cfde4b7f1ccc2516
yohan_mr <yohanmri.21@uom.lk>
2025-07-13 14:26:38 +0530

    Appointments_LawyerScreen

--- admin/src/pages/Lawyer/LawyerAppointment.jsx
+++ admin/src/pages/Lawyer/LawyerAppointment.jsx
@@ -6,7 +6,7 @@ import { assets } from '../../assets/assets'
 
 const LawyerAppointment = () => {
 
-    const { dToken, appointments, getAppointments } = useContext(LawyerContext)
+    const { dToken, appointments, getAppointments, completeAppointment, cancelAppointment } = useContext(LawyerContext)
     const { calculateAge, slotDateFormat, currency } = useContext(AppContext)
 
 
@@ -32,7 +32,7 @@ const LawyerAppointment = () => {
                 </div>
 
                 {
-                    appointments.map((item, index) => (
+                    appointments.reverse().map((item, index) => (
                         <div className='flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50' key={index}>
                             <p className='max-sm:hidden'>{index + 1}</p>
                             <div className='flex items-center gap-2'>
@@ -46,10 +46,19 @@ const LawyerAppointment = () => {
                             <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
                             <p>{slotDateFormat(item.slotDate)} | {item.slotTime}</p>
                             <p>{currency} {item.amount}</p>
-                            <div className='flex'>
-                                <img className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
-                                <img className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
-                            </div>
+
+                            {/*Nested turnery operator*/}
+                            {
+                                item.cancelled
+                                    ? <p className='text-red-400 text-xs font-medium'>Cancelled</p>
+                                    : item.isCompleted
+                                        ? <p className='text-green-500 text-xs font-medium'>Completed</p>
+                                        : <div className='flex'>
+                                            <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+                                            <img onClick={() => completeAppointment(item._id)} className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
+                                        </div>
+                            }
+
                         </div>
                     ))
                 }
--- backend/routes/lawyerRoute.js
+++ backend/routes/lawyerRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { appointmentsLawyer, lawyerList, loginLawyer } from "../controllers/lawyerController.js";
+import { appointmentCancel, appointmentComplete, appointmentsLawyer, lawyerList, loginLawyer } from "../controllers/lawyerController.js";
 import authLawyer from "../middlewares/authLawyer.js";
 
 const lawyerRouter = express.Router();
 
 lawyerRouter.get('/list', lawyerList);
 lawyerRouter.post('/login', loginLawyer);
-lawyerRouter.get('/appointments', authLawyer, appointmentsLawyer)
+lawyerRouter.get('/appointments', authLawyer, appointmentsLawyer);
+lawyerRouter.post('/complete-appointment',authLawyer,appointmentComplete)
+lawyerRouter.post('/cancel-appointment',authLawyer,appointmentCancel)
 
 export default lawyerRouter;
