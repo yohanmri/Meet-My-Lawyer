68c05d30ea6f6d911a59fa53d2f719ac7e6e7e79
yohan_mr <yohanmri.21@uom.lk>
2025-07-13 19:37:07 +0530

    Commit_before_modifications

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from 'react'
-import { assets } from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
