73d8b066bdf6f7989ec80540cfc3aa8e8bf9079b
yohan_mr <yohanmri.21@uom.lk>
2025-09-04 18:10:17 +0530

    Sride bar and Login jsx parts changed

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -50,7 +50,7 @@ const Login = () => {
     return (
         <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
             <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
-                <p className='text-2xl font-semibold m-auto'><span className='text-primary'>{state}</span> Login</p>
+                <p className='text-2xl font-semibold m-auto'><span className='text-[#D00C1F]'>{state}</span> Login</p>
                 <div className='w-full'>
                     <p>Email</p>
                     <input onChange={(e) => setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
@@ -59,11 +59,14 @@ const Login = () => {
                     <p>Password</p>
                     <input onChange={(e) => setPassword(e.target.value)} value={password} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
                 </div>
-                <button className='bg-primary text-white w-full py-2 rounded-md text-base'>Login</button>
+                <button className='text-white w-full py-2 rounded-md text-base'
+                    style={{ background: 'linear-gradient(to right, #D00C1F, #6A0610)' }}
+
+                >Login</button>
                 {
                     state === 'Admin'
-                        ? <p>Lawyer Login ? <span className='text-primary underline cursor-pointer' onClick={() => setState('Lawyer')}> Click here</span></p>
-                        : <p>Admin Login ? <span className='text-primary underline cursor-pointer' onClick={() => setState('Admin')}> Click here</span></p>
+                        ? <p>Lawyer Login ? <span className='text-[#6A0610] underline cursor-pointer' onClick={() => setState('Lawyer')}> Click here</span></p>
+                        : <p>Admin Login ? <span className='text-[#6A0610] underline cursor-pointer' onClick={() => setState('Admin')}> Click here</span></p>
                 }
             </div>
         </form>
